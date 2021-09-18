c130227d55a2003b6b130b50fa0e8360be900358
bayaroch <bayaroch@gmail.com>
2021-09-19 01:09:22 +0800

    sort by usercode

--- src/store/account/selectors/index.ts
+++ src/store/account/selectors/index.ts
@@ -3,7 +3,9 @@ import { RootState } from '@store/reducers'
 
 const getState = (state: RootState) => state.account
 
-export const members = createSelector(getState, (state) => state.memberList)
+export const members = createSelector(getState, (state) =>
+  _.sortBy(state.memberList, 'usercode')
+)
 export const paginationMeta = createSelector(
   getState,
   (state) => state.memberMeta
