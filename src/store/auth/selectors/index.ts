2765e714ffa4b1ad7c3edffd0aca901052c7db4e
bayaroch <bayaroch@gmail.com>
2022-01-13 23:23:34 +0800

    fix

--- src/store/auth/selectors/index.ts
+++ src/store/auth/selectors/index.ts
@@ -11,6 +11,7 @@ export const memberType = createSelector(
   authState,
   (state) => state.user?.usertype
 )
-export const category = createSelector(authState, (state) =>
-  helper.categoryCalc(state.user.birthday, state.user.gender)
+export const category = createSelector(
+  authState,
+  (state) => helper.categoryCalc(state.user.birthday, state.user.gender)[0] // selecting only first cat exluded lady future change might be needed sending multiple categories 1, 6 etc
 )
