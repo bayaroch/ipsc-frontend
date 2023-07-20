021a5a4ec11899dcd138a336fd313323f35df6c3
Joy Spark <bayaroch@gmail.com>
2023-07-20 14:59:43 +0800

    fix

--- pages/junior/index.tsx
+++ pages/junior/index.tsx
@@ -1,16 +1,49 @@
 import ContentWrapper from '@components/layout/ContentWrapper'
 import MainLayout from '@components/layout/MainLayout'
 import PageWithLayoutType from '@constants/page'
-import JuniorContainer from '@containers/Pages/JuniorContainer'
 import ContentHeader from '@components/elements/ContentHeader'
+import SideBarMenu from '@components/common/SideBarMenu'
+import { allproducts } from '@constants/featured.course'
+import HTMLParser from '@components/common/HtmlParser'
+import { Box, Grid } from '@mui/material/'
+import useWordpress from '@utils/hooks/useWordpress'
+import { useEffect } from 'react'
 
 const Junior: PageWithLayoutType = () => {
+  const { data, fetchPage } = useWordpress()
+
+  useEffect(() => {
+    fetchPage('3204')
+  }, [])
+
   return (
     <MainLayout isBanner={true} regular={true}>
-      <ContentHeader title={'Жуниор хөтөлбөр'} />
+      <ContentHeader title={data && data.title} />
       <ContentWrapper topSpace={true}>
         <>
-          <JuniorContainer />
+          <Grid container maxWidth={'xl'}>
+            <Grid md={9} sm={12} xs={12} item>
+              <Box
+                sx={{
+                  paddingRight: { lg: '50px', md: '50px', sm: 0, xs: 0 },
+                  paddingBottom: {
+                    lg: '100px',
+                    md: '100px',
+                    sm: '20px',
+                    xs: '20px',
+                  },
+                  textAlign: { sm: 'justify', xs: 'justify' },
+                }}
+              >
+                <div className="content">
+                  {data && <HTMLParser html={data.content} />}
+                </div>
+              </Box>
+            </Grid>
+            <Grid md={3} sm={12} xs={12} item>
+              <SideBarMenu title="Сургалтууд" data={allproducts} />
+            </Grid>
+          </Grid>
         </>
       </ContentWrapper>
     </MainLayout>
